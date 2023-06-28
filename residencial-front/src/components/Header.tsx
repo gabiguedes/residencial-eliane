74b77bec60cd4d4e72f00d0c792bd172529af043
Gabi Guedes <zooiv3rde@gmail.com>
2023-06-28 16:04:40 -0300

    ajustando header

--- residencial-front/src/components/Header.tsx
+++ residencial-front/src/components/Header.tsx
@@ -1,18 +1,10 @@
 import styles from './Header.module.css'
 
-import residencialLogo from '../assets/residencial-eliane-logo.svg'
-
 export function Header() {
-    function handlerEntrar() {
-        console.log('entrei na func de entrar');
-    }
-
+    
     return (
-        <header className={styles.header}>
-            <img src={residencialLogo} alt="Logo do Residencial Eliane" />
+        <header className={styles.header}>        
             <p>Residencial Eliane</p>
-
-            <button onClick={handlerEntrar} title='Entrar'>ENTRAR</button>
         </header>
     )
 }
